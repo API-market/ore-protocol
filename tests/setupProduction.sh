9ceca61665c56a6cffad5dc106b9598e56a06cde
basarrcan <basarrcan@gmail.com>
2020-12-08 20:35:12 +0000

    update chain setup

--- tests/setupProduction.sh
+++ tests/setupProduction.sh
@@ -9,9 +9,9 @@ production_ore_contracts=~/ore-protocol/tests/old-contracts/ore-protocol
 createescrow_path=~/ore-protocol/tests/old-contracts/CreateEscrow
 
 echo 'Create accounts'
-./tests/chain-setup/2.create_accounts.sh $cleos $production_eosio_contracts
+./chain-setup/2.create_accounts.sh $cleos $production_eosio_contracts
 echo 'Vote producer'
-./tests/chain-setup/3.vote.sh $cleos
+./chain-setup/3.vote.sh $cleos
 echo 'Deploy system.ore'
 ./chain-setup/4.oresystem.sh $cleos $production_ore_contracts
 echo 'Deploy createescrow'
